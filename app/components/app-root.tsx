5125d9f5b38bfd2a31d67468df68f4087ddc4d0c
Jerry <ptp_jerry.chou@photons.com.tw>
2024-09-09 13:00:47 +0800

    feat: [INT-43] 新增tg連結 startapp 參數邏輯
    適用 啟動錢包頁跳轉、分享連結

--- app/components/app-root.tsx
+++ app/components/app-root.tsx
@@ -1,4 +1,5 @@
-import { type PropsWithChildren, useEffect } from 'react'
+import { type PropsWithChildren, useEffect, useMemo } from 'react'
+import { useNavigate } from '@remix-run/react'
 import { SDKProvider, useLaunchParams } from '@telegram-apps/sdk-react'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -11,17 +12,24 @@ const manifestUrl =
   'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
 
 export default function AppRoot({ children }: PropsWithChildren) {
+  const navigate = useNavigate()
   useTelegramMock()
 
-  const debug = useLaunchParams(true)?.startParam === 'debug'
+  const startParam = useLaunchParams(true)?.startParam
   // Enable debug mode to see all the methods sent and events received.
-  useEffect(() => {
-    console.log('debug', debug)
+  const debug = useMemo(() => startParam === 'debug', [startParam])
 
-    if (debug) {
+  useEffect(() => {
+    if (startParam === 'debug') {
       import('eruda').then(lib => lib.default.init())
+    } else if (startParam === 'wallet') {
+      navigate('/wallet')
+    } else if (startParam && /^r_/.test(startParam)) {
+      // 實作分享邀請碼功能，預期以某字符開頭 例如r_ 開頭 eg: r_xdgYdr6
+      const referralCode = startParam.replace(/^r_/, '') // 去掉前面的 r_
+      console.log('referralCode: ', referralCode)
     }
-  }, [debug])
+  }, [navigate, startParam])
 
   return (
     <TonConnectUIProvider manifestUrl={manifestUrl}>
