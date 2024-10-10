8408cbb1ca95f538f5d21b1493de4ab629b3ed18
Jerry <ptp_jerry.chou@photons.com.tw>
2024-10-10 22:52:00 +0800

    chore: update api index

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,16 +1,34 @@
 import { apiClient } from './api-client'
+import { Banner } from './codegen/Banner'
+import { Campaign } from './codegen/Campaign'
 import { Customer } from './codegen/Customer'
 import { Games } from './codegen/Games'
 import { Game } from './codegen/Game'
 import { Header } from './codegen/Header'
+import { PacketSetting } from './codegen/PacketSetting'
+import { Packets } from './codegen/Packets'
+import { Packet } from './codegen/Packet'
+import { Public } from './codegen/Public'
+import { System } from './codegen/System'
+import { Tasks } from './codegen/Tasks'
+import { Task } from './codegen/Task'
 import { Team } from './codegen/Team'
 import { Wallet } from './codegen/Wallet'
 
 export const apis = {
+  banner: new Banner(apiClient),
+  campaign: new Campaign(apiClient),
   customer: new Customer(apiClient),
   games: new Games(apiClient),
   game: new Game(apiClient),
   header: new Header(apiClient),
+  packetSetting: new PacketSetting(apiClient),
+  packets: new Packets(apiClient),
+  packet: new Packet(apiClient),
+  public: new Public(apiClient),
+  system: new System(apiClient),
+  tasks: new Tasks(apiClient),
+  task: new Task(apiClient),
   team: new Team(apiClient),
   wallet: new Wallet(apiClient),
   // 其他模塊往下繼續添加
