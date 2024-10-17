86a8d47a9b23ac876a3f558087499ad2333b5af9
Jerry <ptp_jerry.chou@photons.com.tw>
2024-10-17 15:14:17 +0800

    feat: 調整 ProtectedLink

--- app/components/protected-link.tsx
+++ app/components/protected-link.tsx
@@ -6,20 +6,31 @@ interface ProtectedLinkProps extends LinkProps {
   onClick?: (e: MouseEvent<HTMLAnchorElement>) => void
 }
 
-export default function ProtectedLink({ to, className, children, ...rest }: ProtectedLinkProps) {
+export default function ProtectedLink({
+  to,
+  className,
+  children,
+  onClick,
+  ...rest
+}: ProtectedLinkProps) {
   const isLoggedIn = useStore(state => state.isLoggedIn)
   const openNeedLoginDialog = useStore(state => state.openNeedLoginDialog)
 
   const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault() // 阻止默认跳转行为
-    openNeedLoginDialog() // 打开登录对话框
+    if (!isLoggedIn)
+      openNeedLoginDialog() // 打开登录对话框
+    else if (onClick) onClick(e) // 如果有 onClick 回调，则执行
   }
 
-  return isLoggedIn ? (
-    <Link to={to} className={className} {...rest}>
-      {children}
-    </Link>
-  ) : (
+  if (isLoggedIn && to && !onClick)
+    // 如果已登录且没有 onClick 回调，则直接使用 to 跳转
+    return (
+      <Link to={to} className={className} {...rest}>
+        {children}
+      </Link>
+    )
+  return (
     // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <a href="#" className={className} onClick={handleClick}>
       {children}
