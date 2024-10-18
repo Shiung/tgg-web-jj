f457622acbc6b8ef0598d5b7f4665746dee68781
Jerry <ptp_jerry.chou@photons.com.tw>
2024-10-18 23:45:18 +0800

    fix: [INT-673][Game][紅包] 用戶無發送中的紅包，點紅包進入頁顯示為My Share

--- app/routes/lucky-money.list/route.tsx
+++ app/routes/lucky-money.list/route.tsx
@@ -1,5 +1,5 @@
-import { useMemo } from 'react'
-import { Link } from '@remix-run/react'
+import { useEffect, useMemo } from 'react'
+import { Link, useNavigate } from '@remix-run/react'
 import { useInfiniteQuery } from '@tanstack/react-query'
 import qs from 'qs'
 
@@ -15,35 +15,43 @@ import { ListSkeleton } from './skeleton'
 const QUERY_STATE = [1]
 
 export default function LuckyMoneyList() {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } = useInfiniteQuery({
-    queryKey: ['packetsList', QUERY_STATE],
-    queryFn: ({ pageParam = 1 }) =>
-      apis.packets
-        .packetsList(
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          { states: QUERY_STATE, page: pageParam, pageSize: 20 },
-          { paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }) }
-        )
-        .then(res => ({
-          list: res.data.list || [],
-          pagination: {
-            pageSize: res.data.pagination?.pageSize ?? 0,
-            totalPage: res.data.pagination?.totalPage ?? 0,
-            totalRecord: res.data.pagination?.totalRecord ?? 0,
-          },
-        })),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) => {
-      if (lastPage.pagination.totalPage > allPages.length) {
-        return allPages.length + 1
-      }
-      return undefined
-    },
-    retry: false,
-  })
+  const navigate = useNavigate()
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isFetched } =
+    useInfiniteQuery({
+      queryKey: ['packetsList', QUERY_STATE],
+      queryFn: ({ pageParam = 1 }) =>
+        apis.packets
+          .packetsList(
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            { states: QUERY_STATE, page: pageParam, pageSize: 20 },
+            { paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }) }
+          )
+          .then(res => ({
+            list: res.data.list || [],
+            pagination: {
+              pageSize: res.data.pagination?.pageSize ?? 0,
+              totalPage: res.data.pagination?.totalPage ?? 0,
+              totalRecord: res.data.pagination?.totalRecord ?? 0,
+            },
+          })),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage.pagination.totalPage > allPages.length) {
+          return allPages.length + 1
+        }
+        return undefined
+      },
+      retry: false,
+    })
 
   const list = useMemo(() => data?.pages.flatMap(page => page.list) ?? [], [data?.pages])
+
+  useEffect(() => {
+    if (isFetched && !list.length) {
+      navigate('/lucky-money/share')
+    }
+  }, [isFetched, list.length, navigate])
 
   return (
     <div className="-mt-4 flex w-full flex-1 flex-col items-stretch justify-between rounded-[12px] bg-black p-4">
