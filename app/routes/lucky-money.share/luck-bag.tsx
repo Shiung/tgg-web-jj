ada6f58be18f14499cb084570056df0bc5f4b87d
Jerry <ptp_jerry.chou@photons.com.tw>
2024-10-23 15:11:31 +0800

    fix: [INT-679] 修正等於時邏輯

--- app/routes/lucky-money.share/luck-bag.tsx
+++ app/routes/lucky-money.share/luck-bag.tsx
@@ -46,7 +46,7 @@ const LuckBag: React.FC<LuckBagProps> = ({ packetSetting }) => {
   }
 
   const handleMaxValueBlur = () => {
-    if (formMaxValue < formMinValue) {
+    if (formMaxValue <= formMinValue) {
       setValue('maxValue', formMinValue + 1, { shouldValidate: true })
     }
   }
