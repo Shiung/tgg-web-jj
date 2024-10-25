d8d6184ad78416fef3b68a6a97320cef7d997a13
Jerry <ptp_jerry.chou@photons.com.tw>
2024-10-26 00:53:13 +0800

    feat:  調整 rank skeleton

--- app/routes/rank/components/skeleton.tsx
+++ app/routes/rank/components/skeleton.tsx
@@ -10,10 +10,10 @@ const SkeletonBox = ({ isInit }: { isInit?: boolean }) => {
           })}
         </div>
       )}
-      <Skeleton className="aspect-[375/270] w-full rounded-bl-xl" />
-      <div className="flex flex-1 flex-col space-y-4 rounded-t-xl bg-black p-4">
+      <Skeleton className="aspect-[375/270] w-full rounded-b-none rounded-t-xl" />
+      <div className="z-10 -mt-4 flex flex-1 flex-col space-y-4 rounded-t-xl bg-black p-4">
         {Array.from({ length: 5 }, (_, idx) => {
-          return <Skeleton key={idx} className="flex-1 flex-shrink-0" />
+          return <Skeleton key={idx} className="flex-[0_0_54px]" />
         })}
       </div>
     </div>
